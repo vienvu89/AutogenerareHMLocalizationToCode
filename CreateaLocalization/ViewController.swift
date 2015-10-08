92faf1c9fa32cfb3941533f7166b0c75b9f7df82
Vien Vu <lamvienbk@gmail.com>
2015-10-08 16:00:27 +0700

    Initial Commit

---
+++ CreateaLocalization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreateaLocalization
+//
+//  Created by Vien Vu  on 10/8/15.
+//  Copyright © 2015 VienVu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
